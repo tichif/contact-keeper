b5b68de5f7fac00caabadb160ea60f9972199f55
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-07-08 09:34:14 -0400

    Hash the password and register the user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { body, validationResult } = require('express-validator');
 
 // User Model
@@ -18,12 +19,39 @@ router.post(
       'Please enter a valid password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
-    res.send(req.body);
+
+    const { name, email, password } = req.body;
+
+    try {
+      // Verify if a user already sign up with this email
+      let user = await User.findOne({ email }); // or { email: email }
+      if (user) {
+        res.status(404).json({ msg: 'User already exist' });
+      }
+
+      // Create a new User
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // hash the password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save the user in the database
+      await user.save();
+      res.send('User saved');
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
