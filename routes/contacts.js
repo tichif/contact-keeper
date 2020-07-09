66cb00ae57ae00c18fd48839f6c36cf95f0b49c6
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-07-08 22:09:44 -0400

    Create Add Contact Route

--- routes/contacts.js
+++ routes/contacts.js
@@ -13,7 +13,7 @@ const Contact = require('../models/Contact');
 // @access  Private
 router.get('/', auth, async (req, res) => {
   try {
-    const contacts = await Contact.find({ user: req.body.id }).sort({
+    const contacts = await Contact.find({ user: req.user.id }).sort({
       date: -1,
     });
     res.json(contacts);
@@ -26,9 +26,36 @@ router.get('/', auth, async (req, res) => {
 // @route   POST /api/contacts
 // @desc    Add new contact
 // @access  Private
-router.post('/', (req, res) => {
-  res.send('Add new contact');
-});
+router.post(
+  '/',
+  [auth, [body('name', 'Name is required').notEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      });
+
+      // Save the contact into the DB
+      const contact = await newContact.save();
+
+      res.json(contact);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 // @route   PUT /api/contacts/:id
 // @desc    Update Contact
