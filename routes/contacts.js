7f4a7c325a2d20fb9ad6af9659087b4d45f1eb24
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-07-08 21:46:17 -0400

    Create Contac model and Get Contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const config = require('config');
+const auth = require('../middleware/auth');
+
+// User Model
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route   GET /api/contacts
 // @desc    Get all users contacts
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Get all contacts');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.body.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   POST /api/contacts
